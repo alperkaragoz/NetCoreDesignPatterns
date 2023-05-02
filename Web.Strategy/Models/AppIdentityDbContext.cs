1abb3f2c3186b5c70a69e886205d44ee4836b8af
Alper Karagöz <alperkaragoz@outlook.com>
2023-05-02 23:19:39 +0300

    Create Base Project

---
+++ Web.Strategy/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        // options'u Program.cs'te dolduracağız.(ConnectionStrings vs..)
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
